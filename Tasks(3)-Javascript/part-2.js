2301ab77c9b7fa9391ed170acc827a4aceee6e31
simonakomarovska <simonak399@gmail.com>
2023-12-01 00:59:54 +0100

    3 homework

---
+++ Tasks(3)-Javascript/part-2.js
@@ -0,0 +1,12 @@
+
+
+// Antras variantas
+
+// Kategorija 1: 0 – 5. skaičius dauginamas iš 3
+// Kategorija 2: 6 – 15. iš skaičiaus atsiima 4 ir rezultatas pakeliamas kvadratu
+// Kategorija 3: 16 - 30 ir dalijasi iš 5. Prie skaičiaus pridedame 10
+// Kategorija 4: 31 - 45 ir skaičius nelyginis. Skaičių atimame iš 100
+// Kategorija 5: 46 – 99 ir skaičiai pasibaigiantys 6 arba 7. Skaičių daliname iš 4
+
+
+
