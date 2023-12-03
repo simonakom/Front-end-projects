218c94aa224d32003961b5cd93ad5cfdf57b6a8a
simonakomarovska <simonak399@gmail.com>
2023-12-03 22:48:43 +0100

    variables

--- JS - variables/(6-10)/script.js
+++ JS - variables/(6-10)/script.js
@@ -44,6 +44,33 @@ console.log(`Zero's - ${countZero}\tOne's - ${countOne}\tTwo's - ${countTwo}`);
 // console.log("One's - ", countOne); 
 // console.log("Two's - ", countTwo);
 
+
+ //7 Task with Arrays
+// let kint = [rand(0, 2), rand(0, 2), rand(0, 2), rand(0, 2)];
+// console.log(kint);
+
+// //.fill(x) užpildo visus masyvo elementus tam tikra reikšme
+// let count = new Array(3).fill(0);
+
+// if (kint[0] === 0) count[0]++;
+// else if (kint[0] === 1) count[1]++;
+// else count[2]++;
+
+// if (kint[1] === 0) count[0]++;
+// else if (kint[1] === 1) count[1]++;
+// else count[2]++;
+
+// if (kint[2] === 0) count[0]++;
+// else if (kint[2] === 1) count[1]++;
+// else count[2]++;
+
+// if (kint[3] === 0) count[0]++;
+// else if (kint[3] === 1) count[1]++;
+// else count[2]++;
+
+// console.log(`Skaičių 0 - ${count[0]}\t 1 - ${count[1]}\t 2 - ${count[2]}`);
+
+
  
 //8 Task
 let one = rand (0,4),
@@ -122,3 +149,8 @@ console.log(typeof result);
 //  String.fromCharCode(rand(97, 122)) + 
 //  String.fromCharCode(rand(97, 122))
 //  )
+
+
+
+
+
