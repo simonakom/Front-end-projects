450837e318d35ea5e1c33ab89a9d5d959d7d9ee0
simonakomarovska <simonak399@gmail.com>
2023-12-05 22:45:13 +0100

    variables

--- JS - variables/(11-12)/script.js
+++ JS - variables/(11-12)/script.js
@@ -11,10 +11,21 @@ let finalText = yearsText.replaceAll('{{satellite}}', 'moon');
 console.log(finalText);
 
 
-//12 Task
+// //12 Task
 
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function newFunction (min, max) {
+    let randomFull = rand(min * 100, max * 100) / 100; // Gautas skaičius paverčiamas į skaičių su dviem skaitmenimis po kablelio (Tai daroma panaudojant dalinimą ir pan. matematinius veiksmus). Veliau yra dalinamas iš 100 kad gauti oroginalu skaiciu.
+    return randomFull.toFixed(2); //toFixed(2) nustato, jog rezultatas turėtų būti su dviem skaitmenimis po kablelio
+}
+
+let example = newFunction (0,4);
+    console.log(example);
+
+
+
 
 
