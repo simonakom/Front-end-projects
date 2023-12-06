dc4c8f6975f83ccbd7ded4cef59be869093a9a40
simonakomarovska <simonak399@gmail.com>
2023-12-06 12:26:04 +0100

    loops

--- JS - loops/(1-6)/script.js
+++ JS - loops/(1-6)/script.js
@@ -2,14 +2,12 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 // 1 task
 //Naudokite funkcija rand(). Sugeneruokite atsitiktinį skaičių nuo 1 iki 6 ir jį atspausdinkite atitinkame h tage. Pvz skaičius 3- rezultatas: <h3>3</h3>
 
 let numb = rand(1,6);
 console.log (numb)
 document.querySelector("#result").innerText = `Rezultatas: ${numb}`;
-
 
 // 2 task
 // //Naudokite funkcija rand(). Atspausdinkite 3 skaičius nuo -10 iki 10. Skaičiai mažesni už 0 turi būti žali, 0 - raudonas, didesni už 0 mėlyni.
@@ -27,13 +25,13 @@ for (let i = 0; i < 3; i++) {
         color= 'blue';
     }
 
-    document.write('<p class= "' + color + ' ">' + randomNumber + '</p>' );
+    document.write(`<b class= "${color}">  ${randomNumber} </b><br>`);
+    // document.write('<p class="'+ color+'">'   + randomNumber + '</p>' );
+    document.write('<br>')
 }
-
 
 // 3 task
 //  /*Sukurkite kintamąjį su stringu: “An American in Paris”. Jame ištrinti visas balses. Rezultatą atspausdinti. Kodą pakartoti su stringais: “Breakfast at Tiffany's”, “2001: A Space Odyssey” ir “It's a Wonderful Life”.*/
-
 
  let text = ["An American in Paris",
                     "Breakfast at Tiffany's",
@@ -44,7 +42,7 @@ for (let i = 0; i < 3; i++) {
         let result = text[i].replace(/[aeiou]/gi, '');
         document.write(result+ "</br>"+"</br>");
     }
-
+    document.write('<br>')
 
 
 // 4 task
@@ -68,26 +66,42 @@ for (let i = 0; i < 3; i++) {
         }
         document.write(`<span style="color: ${color};">${xnumber} </span>`);
      }
- 
-  
-     document.write("<br>"+ "Skaičių didesnių už 150 yra:"+ "\n" + x +"<br>")
 
+     document.write("<br>"+ "Skaičių didesnių už 150 yra:"+ "\n" + x)
+     document.write('<br>')
+     document.write('<br>')
 
 // 5 task
 /*Vienoje eilutėje atspausdinkite visus skaičius nuo 1 iki 3000, kurie dalijasi iš 77 be liekanos. Skaičius atskirkite kableliais. Po paskutinio skaičiaus kablelio neturi būti. Jeigu reikia, panaudokite css, kad visi rezultatai matytųsi ekrane.*/
 
-let sk = "";
+
+let sk = [];
 
 for (var i = 1; i <= 3000; i++) {
     if (i % 77 === 0) {
-        sk += i;
-
-        if (i < 3000) {
-            sk += ", ";
-        }
+        sk.push(i);
     }
 }
-document.write("<br>"+sk+"<br>"+"<br>");
+
+document.write(sk.join(', '));
+document.write('<br>')
+document.write('<br>')
+
+
+// // Su kableliu po paskutinio skaiciaus
+// let sk = "";
+
+// for (var i = 1; i <= 3000; i++) {
+//     if (i % 77 === 0) {
+//         sk += i;
+
+//         if (i < 3000) {
+//              sk += ', ';
+//         }
+//     }
+// }
+// document.write("<br>"+sk+"<br>"+"<br>");
+
 
 
 // 6 task
