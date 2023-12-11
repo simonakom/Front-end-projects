3e209a49f06675e3682778da1431a2c4f1174862
simonakomarovska <simonak399@gmail.com>
2023-12-12 00:11:17 +0100

    Array

--- JS - array/2023.12.11/script.js
+++ JS - array/2023.12.11/script.js
@@ -113,3 +113,42 @@ let average = total / numbersArray.length;
 console.log(`10. Average: ${average}`);
 
 
+console.log(`--------------------------------------------------------------------------`);
+
+/*
+1.Sukurti funkciją, kuri generuoja x kiekį atsitiktinių string'ų, sudarytą iš strLength simbolių. Generavimas nuo A iki Z. Sukurti masyvą iš 100 elementų, sudarytą iš String'ų, kurių ilgis yra 4 simboliai;
+2. Patikrinti, ar String masyve egzistuoja reikšmių, prasidedančių bei užsibaigiančių raide A. Jei taip - pranešti kokia šio elemento pozicija masyve bei  reikšmė;
+3. Išfiltruokite visas masyvo reikšmes, pasidedančias raidėmis: 'X', 'M', 'K'
+4. Išfiltruokite visas masyvo reikšmes, kurių viduriniai du simboliai yra vienodi. Sukurkite išfiltruotų reikšmių masyvą;
+5. Jei išfiltruotų reikšmių masyve reikšmių mažiau nei 3 - rikiuoti didėjančia, kitu atveju - rikiuoti mažėjančia tvarka.
+6. Kiekvienai stringų masyvo reikšmei pridėti po dar vieną atsitiktinę raidę gale;
+7. Kiekvieną stringų masyvo elementų reikšmę išrikiuoti pagal abėcėlę didėjančia tvarka
+8. Sukurkite naują masyvą atsitiktinėms Sring'ų reikšmėms generuoti. Sugeneruokite atsitiktines String reikšmes iš 4 simbolių tol, kol jame bus žodis XMAS; Išveskite, kiek kartų reikėjo generuoti reikšmes kol buvo gautas toks žodis.
+9. Išrikiuokite masyvą priešinga nei abecelės tvarka
+10. Atraskite, kurioje masyvo pozicijoje randasi žodis 'XMAS'
+*/
+
+
+// 1.Sukurti funkciją, kuri generuoja x kiekį atsitiktinių string'ų, sudarytą iš strLength simbolių. Generavimas nuo A iki Z. Sukurti masyvą iš 100 elementų, sudarytą iš String'ų, kurių ilgis yra 4 simboliai;
+
+const generateRandomString = (length) => {
+    let strLength ="";
+    for (let i = 0; i < length; i++){
+        strLength+= String.fromCharCode(rand(65, 90));
+    } 
+    return strLength;
+}
+console.log(`1. String:\n ${generateRandomString(4)}.`)
+
+
+const newStringArray = [];
+for (let i = 0; i<100; i++){
+    newStringArray.push(generateRandomString(4));
+}
+console.log(`1. Array:\n ${newStringArray}.`)
+
+
+// 2. Patikrinti, ar String masyve egzistuoja reikšmių, prasidedančių bei užsibaigiančių raide A. Jei taip - pranešti kokia šio elemento pozicija masyve bei  reikšmė;
+
+
+
