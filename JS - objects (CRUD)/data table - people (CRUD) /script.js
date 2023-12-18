890db5ae2b728c6ff31013c38108c6999624d89a
simonakomarovska <simonak399@gmail.com>
2023-12-18 22:39:35 +0100

    data table - people

--- JS - objects (CRUD)/data table - people (CRUD) /script.js
+++ JS - objects (CRUD)/data table - people (CRUD) /script.js
@@ -1,6 +1,9 @@
 const people = []; //An array to store information about people.
-let currentNumeration= 1; //A variable to keep track of the current numeration for each person.
 const allowedNationalities = ['US', 'CANADA', 'UK', 'AUSTRALIA', 'FRANCE', 'GERMANY', 'JAPAN'];
+
+                                 // *currentNumeration disabeled as after deleting 2no. contact, new contact should be have 2id but it has 3id. Numeration is wrong: 1,3,4,5...
+                                 // *let currentNumeration= 1; //A variable to keep track of the current numeration for each person.
+
 
 //selecting HTML elements 
 const buttonElement = document.querySelector ("#add-button");
@@ -18,16 +21,13 @@ const updateResult = document.querySelector (".update-result");
 
 //CREATE: When "Add" is pressed: 
 buttonElement.addEventListener("click", ()=>{
-    
     //When the "Add" button is clicked, a new person object is created from the form inputs.
     const person = {};
     person.firstName = document.getElementById(`firstNameInput`).value;
     person.lastName = document.getElementById(`lastNameInput`).value;
     person.age = document.getElementById(`ageInput`).value;
-    // person.nationality = document.getElementById(`nationalityInput`).value.toUpperCase();
     person.nationality = document.getElementById("nationalityInput").value.toUpperCase() 
-    console.log (person);
-
+    // console.log (person);
 
     // Validations:
     if (!person.firstName || !person.lastName || !person.age || !person.nationality) {
@@ -62,7 +62,7 @@ buttonElement.addEventListener("click", ()=>{
         addResult.style.display = 'none';
     }
 
-     // Duplicate person check is performed based on contact details.
+     // Validations: Duplicate person check is performed based on contact details.
      const duplicatePerson = people.find((existingPerson) =>
         existingPerson.firstName.toLowerCase() === person.firstName.toLowerCase() &&
         existingPerson.lastName.toLowerCase() === person.lastName.toLowerCase() &&
@@ -74,17 +74,19 @@ buttonElement.addEventListener("click", ()=>{
         addResult.innerText = `A person with the same contact details already exists!`;
         addResult.style.display = 'block';
         return;
-        
     } 
+                                    // *currentNumeration disabeled as after deleting 2no. contact, new contact should be have 2id but it has 3id. Numeration is wrong: 1,3,4,5... Instead using (person.number = people.length + 1).
+                                    // *person.number = currentNumeration; // assigns a number property to the person object.
+                                    // *currentNumeration++; // After assigning the number to the current person, currentNumeration is incremented by 1.
 
-        // If all checks pass, the person is added, and the table is updated.
-        people.push(person);//adds the person object to the people array.
+        // This will assign a unique number to each person based on the length of the people array. The +1 is added because array indices start from 0, but you want to display a human-readable number starting from 1.
+        person.number = people.length + 1;
 
-        person.number = currentNumeration; // assigns a number property to the person object.
-        currentNumeration++; // After assigning the number to the current person, currentNumeration is incremented by 1. 
+        // If all checks pass, the person object is added to people array, and the table (generateTableContent) is updated.
+        people.push(person);
 
         generateTableContent (people); //calls the generateTableContent function, passing the updated people array as an argument. The purpose of this function is to generate HTML content for displaying the list of people in a table. 
-        formElement.reset(); //resets the form (formElement) after adding a person. It clears the input fields in the form,
+        formElement.reset(); //resets the form (formElement) after adding a person. It clears the input fields in the form.
 }); 
 
 //READ: A function to generate HTML content for displaying the list of people in a table.
@@ -101,22 +103,21 @@ function generateTableContent (people){
         </tr>`;
     }
 
-    const tbody =document.querySelector(`table tbody`)
+    const tbody = document.querySelector(`table tbody`)
     tbody.innerHTML = dynamicHTML;
 }
 
 //DELETE: When "delete" is pressed: 
 deleteElement.addEventListener("click", ()=>{
     const removeElementImput = document.querySelector ("#numberInput"); //representing the number of the person to be deleted.
-    let num = +removeElementImput.value;//It retrieves the numerical value from the input and stores it in the variable num.
+    let num = removeElementImput.value;//It retrieves the numerical value from the input and stores it in the variable num.
     removeElementImput.value = ''; //reiksmiu nusinulinimas
     deleteResult.style.display = "none";
 
-    // findindex grazina indeksa pagal elelemnto reiksme. Jei toks nebuvo rastas, garzina -1.
-    let foundIndexDelete = people.findIndex((person) => person.number == +num) //The findIndex method is used to find the index of the person in the people array whose number matches the input value (num).If no matching person is found, foundIndexDelete is set to -1.
+    // findindex grazina indeksa pagal elelemnto reiksme. Jei toks nebuvo rastas, grazina -1.
+    let foundIndexDelete = people.findIndex((person) => person.number == num) //The findIndex method is used to find the index of the person in the people array whose number matches the input value (num).If no matching person is found, foundIndexDelete is set to -1.
 
-
-    //validation (checks are performed on the input value)
+    //validation: checks are performed on the input value.
     if (!num || isNaN(num)) {
         deleteResult.innerText = "Please enter a number";
         deleteResult.style.display = "block";
@@ -133,7 +134,6 @@ deleteElement.addEventListener("click", ()=>{
         deleteResult.style.display = 'none';
     }
 
-
     people.splice(foundIndexDelete,1);//If the validation checks pass, it uses splice to remove one element at the found index from the people array.
     generateTableContent(people);//It then calls the generateTableContent function to update the table display with the modified people array.
 }); 
@@ -141,16 +141,14 @@ deleteElement.addEventListener("click", ()=>{
 //declares a variable outside of any function, making it accessible globally. This variable will be used to store the index of the person with the specified number when the "Find" button is clicked.
 let foundIndexUpdate;
 
-
 //When "find" is pressed: 
 findElement.addEventListener("click", ()=>{
-    
     //Retrieves the value from the input field (numberInputUpdate) representing the number of the person to be found.
     const updateElementImput = document.querySelector("#numberInputUpdate");
-    let updateNumber = +updateElementImput.value;
+    let updateNumber = updateElementImput.value;
 
 //Uses the findIndex method to search for the person with the specified number in the people array and assigns the index to foundIndexUpdate.
-foundIndexUpdate = people.findIndex((person) => person.number == updateNumber); // kazkur pasikete type tad reikia tik == kad updatinti kelis kartus.(Tikrinti: console.log(foundIndexUpdate)/console.log(people)/console.log(updateNumber))
+foundIndexUpdate = people.findIndex((person) => person.number == updateNumber); // kazkur pasikete type tad reikia tik == kad updatinti kelis kartus.(seip type neturetu keistis....)(Tikrinti: console.log(foundIndexUpdate)/console.log(people)/console.log(updateNumber))
 
     //validation
     if (!updateNumber || isNaN(updateNumber)) {
@@ -175,23 +173,21 @@ foundIndexUpdate = people.findIndex((person) => person.number == updateNumber); 
     document.getElementById("updateNationalityInput").value = foundPerson.nationality;
 
     updateForm.style.display = 'block';//show form
-    findElement.style.display = 'none';//hide update button
+    findElement.style.display = 'none';//hide find button
     updateElement.style.display = 'block';//show update button
-    updateResult.style.display = "none";
+    updateResult.style.display = "none";//hide alert notes
 }); 
-
 
 //When "update" is pressed: 
 updateElement.addEventListener("click", () => {
-    
-    const updatedPerson = {};//An empty object updatedPerson is created to represent the updated person.
+    const updatedPerson = {};//An empty object is created to represent the updated person.
     //The properties of this object are populated with values from the update form inputs.
     updatedPerson.number = document.querySelector("#numberInputUpdate").value
     updatedPerson.firstName = document.getElementById("updateFirstNameInput").value;
     updatedPerson.lastName = document.getElementById("updateLastNameInput").value;
     updatedPerson.age = document.getElementById("updateAgeInput").value;
     updatedPerson.nationality = document.getElementById("updateNationalityInput").value.toUpperCase();
-    console.log (updatedPerson);
+    // console.log (updatedPerson);
 
    //Validation
     if (!updatedPerson.firstName || !updatedPerson.lastName || !updatedPerson.age || !updatedPerson.nationality) {
@@ -240,12 +236,8 @@ updateElement.addEventListener("click", () => {
     document.getElementById("updateNationalityInput").value = "";
 
     updateForm.style.display = 'none'; //hide the update form
-    findElement.style.display = 'block'; //show the update form
+    findElement.style.display = 'block'; //show find button
     updateElement.style.display = 'none'; //hide update button
 });
-
-
-
-
 
 
