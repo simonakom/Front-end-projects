c7d167ecea506de482b64460abc6572224ec5a7e
simonakomarovska <simonak399@gmail.com>
2023-12-27 13:07:05 +0200

    data table - people

--- JS - objects (CRUD)/data table - people (CRUD) /script.js
+++ JS - objects (CRUD)/data table - people (CRUD) /script.js
@@ -361,13 +361,8 @@ updateElement.addEventListener("click", () => {
 
          const updatedFirstName = document.getElementById("updateFirstNameInput").value;
          const updatedLastName = document.getElementById("updateLastNameInput").value;
-         const updatedAge = document.getElementById("updateAgeInput").value;
-         const updatedNationality = document.getElementById("updateNationalityInput").value.toUpperCase();
-         const updatedImage = document.getElementById("updateImageInput").value;
          
-         const duplicatePerson = people.find(
-            (existingPerson, index) =>
-                index !== foundIndexUpdate &&
+         const duplicatePerson = people.find((existingPerson) =>
                 existingPerson.firstName.toLowerCase() === updatedFirstName.toLowerCase() &&
                 existingPerson.lastName.toLowerCase() === updatedLastName.toLowerCase()
         );
@@ -380,6 +375,8 @@ updateElement.addEventListener("click", () => {
         } else {
             updateResult.style.display = 'none';
         }
+
+
 
 
     // Update the person in the people array at the index foundIndexUpdate.
