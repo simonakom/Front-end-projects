d51f133037ccd78f63a031abf6810ef9348a5232
simonakomarovska <simonak399@gmail.com>
2023-12-30 17:03:57 +0200

    Vowel App

--- Additional tasks/Workshop 2023.12.07 (JS-vocals counter) /script.js
+++ Additional tasks/Workshop 2023.12.07 (JS-vocals counter) /script.js
@@ -2,34 +2,29 @@ const btn = document.querySelector(".btn")
 const word = document.querySelector(".input-text")
 const result = document.querySelector(".result")
 
-
 btn.addEventListener("click", countVowel)
 
-function countVowel () {
+function countVowel() {
     let vowelCount = 0;
-    let wordValue = word.value;
+    let wordValue = word.value.toUpperCase();
 
     for (let i = 0; i < wordValue.length; i++) {
-        let letter =  wordValue[i];
-        console.log(letter)
+        let letter = wordValue[i];
 
-        if (letter.match(/([a,e,o,u,i])/)) {
+        if (letter.match(/[aeiou]/i)) {
             vowelCount++
         }
     }
 
-    result.innerHTML=`${word.value.toUpperCase()} has ${vowelCount} vowels`
-
-
     if (word.value === "") {
         result.style.display = 'block';
-		result.innerText = `Please enter a word`;
-        
+        result.innerText = `Please enter a word`;
         return;
     }
-
     if (/^\d+$/.test(word.value) || /[^a-zA-Z]/.test(word.value)) {
         result.innerText = `Please enter a valid word \n (only letters are allowed)`;
         return;
     }
+
+    result.innerHTML = `${wordValue} has ${vowelCount} vowels`;
 }
