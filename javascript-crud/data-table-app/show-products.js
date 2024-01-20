fa381dc1a98b539e714ecbab5d815ebe81a80865
simonakomarovska <simonak399@gmail.com>
2024-01-20 21:34:16 +0100

    data table app

--- javascript-crud/data-table-app/show-products.js
+++ javascript-crud/data-table-app/show-products.js
@@ -2,23 +2,22 @@
 const products = JSON.parse(localStorage.getItem("products"));
 console.log(products)
 
-
 function generateStars(rating) { // rating parametras - skaicius 0-5
-    const  fullStar = `<i class="bi bi-star-fill"></i>`,
-	        halfStar = `<i class="bi bi-star-half"></i>`,
-	        emptyStar = '<i class="bi bi-star"></i>';
+    const fullStar = `<i class="bi bi-star-fill"></i>`,
+        halfStar = `<i class="bi bi-star-half"></i>`,
+        emptyStar = '<i class="bi bi-star"></i>';
 
-    let dynamicHTML ="";
-    const countOfFullStars = Math.floor(rating), //tiek kokia yra sveikoji dalis (Math.floor) skaiciaus (rating)
-            halfStarShouldExist = rating - countOfFullStars >= 0.5, //True/False: jei nesveikoji skaiciaus dalis didesne nei 0.50 -  tada rodoma 4 su puse zvaizges
-            emptyStarCount =  halfStarShouldExist ? 4-countOfFullStars : 5-countOfFullStars  // Bolean: jei (halfStarShouldExist) yra true, tada 4-c(ountOfFullStars), priesingu atveju jei puses zvaigzdutes nededam tada is 5-c(ountOfFullStars)
+let dynamicHTML ="";
+const countOfFullStars = Math.floor(rating), //tiek kokia yra sveikoji dalis (Math.floor) skaiciaus (rating)
+        halfStarShouldExist = rating - countOfFullStars >= 0.5, //True/False: jei nesveikoji skaiciaus dalis didesne nei 0.50 -  tada rodoma 4 su puse zvaizges
+        emptyStarCount =  halfStarShouldExist ? 4-countOfFullStars : 5-countOfFullStars  // Bolean: jei (halfStarShouldExist) yra true, tada 4-c(ountOfFullStars), priesingu atveju jei puses zvaigzdutes nededam tada is 5-c(ountOfFullStars)
 
-    //prideti zvaigzdes i dynamicHTML
-    for (let i=0; i<countOfFullStars; i++) dynamicHTML+=fullStar;
-    if (halfStarShouldExist) dynamicHTML+= halfStar;
-    for (let i =0; i<emptyStarCount; i++) dynamicHTML+= emptyStar;
+//prideti zvaigzdes i dynamicHTML
+for (let i=0; i<countOfFullStars; i++) dynamicHTML+=fullStar;
+if (halfStarShouldExist) dynamicHTML+= halfStar;
+for (let i =0; i<emptyStarCount; i++) dynamicHTML+= emptyStar;
 
-    return dynamicHTML;
+return dynamicHTML;
 }
 
 function showProducts() {
@@ -47,7 +46,7 @@ function showProducts() {
             </div>
         </div>`;
 	}
-        //Prie elemnto su klase container reikia prideti dynamicHTML:
+    //Prie elemnto su klase container reikia prideti dynamicHTML:
 	document.querySelector(".container").innerHTML = dynamicHTML;
 }
 
