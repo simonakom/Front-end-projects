148d3069fe0db6124b6ecbd39952afaa4f9e03a6
simonakomarovska <simonak399@gmail.com>
2024-01-20 23:52:48 +0100

    bmi

--- tasks-additional/bmi-calculator/script.js
+++ tasks-additional/bmi-calculator/script.js
@@ -4,11 +4,9 @@ const   height= document.querySelector(".height"),
         result = document.querySelector (".result"),
         reset = document.querySelector (".reset");
 
-
 // attach an event listener to a click event on an HTML element. The event listener is set to execute a function named calBMI when the click event occurs.
 calculate. addEventListener("click", calBMI);
 reset.addEventListener("click", resetForm);
-
 
 // This declares a function named calBMI that takes an event object (e) as its parameter.Event objects are automatically passed to event handler functions and contain information about the event, such as its type, target element, and any additional data related to the event.
 function calBMI (e) {
@@ -29,28 +27,26 @@ function calBMI (e) {
         let bmi = (weightValue/Math.pow(heightMeters, 2)).toFixed(2)
         calculate.style.display = 'none';
         reset.style.display = 'block';
-    
-                    if (bmi <= 18.5){
-                        showResult (`Underweight: <span>${bmi}</span>`, "#55bbdd8d")
-                    }
-                    if (bmi >= 18.5 && bmi <= 25){
-                        showResult (`Normal: <span>${bmi}</span>`, " #85b18496")
-                    }
-                    if (bmi >= 25 && bmi <= 30){
-                        showResult (`Overweight: <span>${bmi}</span>`, "#f5c0397e")
-                    }
-                    if (bmi >= 30){    
-                        showResult (`Obese: <span>${bmi}</span>`, "#c97262aa")
-                    }
+
+        if (bmi <= 18.5){
+            showResult (`Underweight: <span>${bmi}</span>`, "#55bbdd8d")
+        }
+        if (bmi >= 18.5 && bmi <= 25){
+            showResult (`Normal: <span>${bmi}</span>`, " #85b18496")
+        }
+        if (bmi >= 25 && bmi <= 30){
+            showResult (`Overweight: <span>${bmi}</span>`, "#f5c0397e")
+        }
+        if (bmi >= 30){    
+            showResult (`Obese: <span>${bmi}</span>`, "#c97262aa")
+        }
     }
     result.style.display = 'block';
 }
-
 function showResult (value, color ) {
     result.style.backgroundColor = color
     return result.innerHTML = value
 }
-
 function resetForm() {
    location.reload();
 
