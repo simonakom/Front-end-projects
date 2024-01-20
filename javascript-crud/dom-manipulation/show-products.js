a061b541490c583f7b4e07e7518af6e8e5d298c4
simonakomarovska <simonak399@gmail.com>
2024-01-20 20:21:27 +0100

    dom manipulation

--- javascript-crud/dom-manipulation/show-products.js
+++ javascript-crud/dom-manipulation/show-products.js
@@ -1,7 +1,6 @@
 //patikrinti ar veikia ir matome objektus
 console.log(products);
 
-
 function generateStars(rating) { // rating parametras - skaicius 0-5
     const  fullStar = `<i class="bi bi-star-fill"></i>`,
 	        halfStar = `<i class="bi bi-star-half"></i>`,
@@ -9,14 +8,13 @@ function generateStars(rating) { // rating parametras - skaicius 0-5
 
     let dynamicHTML ="";
     const countOfFullStars = Math.floor(rating), //tiek kokia yra sveikoji dalis (Math.floor) skaiciaus (rating)
-            halfStarShouldExist = rating - countOfFullStars >= 0.5, //True/False: jei nesveikoji skaiciaus dalis didesne nei 0.50 -  tada rodoma 4 su puse zvaizges
-            emptyStarCount =  halfStarShouldExist ? 4-countOfFullStars : 5-countOfFullStars  // Bolean: jei (halfStarShouldExist) yra true, tada 4-c(ountOfFullStars), priesingu atveju jei puses zvaigzdutes nededam tada is 5-c(ountOfFullStars)
+        halfStarShouldExist = rating - countOfFullStars >= 0.5, //True/False: jei nesveikoji skaiciaus dalis didesne nei 0.50 -  tada rodoma 4 su puse zvaizges
+        emptyStarCount =  halfStarShouldExist ? 4-countOfFullStars : 5-countOfFullStars  // Bolean: jei (halfStarShouldExist) yra true, tada 4-c(ountOfFullStars), priesingu atveju jei puses zvaigzdutes nededam tada is 5-c(ountOfFullStars)
 
     //prideti zvaigzdes i dynamicHTML
     for (let i=0; i<countOfFullStars; i++) dynamicHTML+=fullStar;
     if (halfStarShouldExist) dynamicHTML+= halfStar;
     for (let i =0; i<emptyStarCount; i++) dynamicHTML+= emptyStar;
-
     return dynamicHTML;
 }
 
@@ -46,7 +44,7 @@ function showProducts() {
             </div>
         </div>`;
 	}
-        //Prie elemnto su klase container reikia prideti dynamicHTML:
+    //Prie elemnto su klase container reikia prideti dynamicHTML:
 	document.querySelector(".container").innerHTML = dynamicHTML;
 }
 
